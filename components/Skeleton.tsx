314566b259671092bd40c7fa894435239e5eaa5c
LuanGrod <luan14rodrigues17@gmail.com>
2024-04-15 19:48:44 -0300

    corrigido o skeleton

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -1,22 +1,44 @@
 import ContentLoader from "react-content-loader"
-
+import { motion } from "framer-motion";
 interface SkeletonProps {
 
 }
 
-export default async function Skeleton({  }: SkeletonProps) {
+export default async function Skeleton({ }: SkeletonProps) {
+  const skeleton = [0, 0, 0, 0, 0, 0, 0, 0];
+
   return (
-    <div style={{ borderRadius: 50 }}>
-      <ContentLoader
-        speed={4}
-        width={400}
-        height={460}
-        viewBox="0 0 400 460"
-        backgroundColor="#c4c4c480"
-        foregroundColor="#ffffff80"
+    <div style={{
+      flexGrow: 10,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <div
+        style={{
+          width: "938px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: "31px 22px",
+        }}
       >
-        <rect x="0" y="0" rx="12" ry="12" width="218" height="285" />
-      </ContentLoader>
-    </div>
+        {
+          skeleton.map((index) => (
+            <ContentLoader 
+            speed={0.3}
+            width={218}
+            height={285}
+            viewBox="0 0 218 285"
+            backgroundColor="#c4c4c4"
+            foregroundColor="#ffffff"
+          >
+            <rect x="0" y="0" rx="12" ry="12" width="218" height="285" />
+          </ContentLoader>
+          ))
+        }
+      </div>
+    </div >
   );
 }
